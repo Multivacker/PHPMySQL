5a855bc4ed4e38c7568cfce598dd54f47e5d55e4
NahuelEz2 <nec2@live.com.ar>
2018-04-13 15:00:40 -0300

    Update pagina_insertar_usuarios.php

--- encriptar/pagina_insertar_usuarios.php
+++ encriptar/pagina_insertar_usuarios.php
@@ -16,7 +16,7 @@
 				
 	try{
 
-		$base=new PDO('mysql:host=localhost; dbname=pruebas', 'root', 'BrizuelaMySQL1999');
+		$base=new PDO('mysql:host=localhost; dbname=pruebas', 'root', '');
 		
 		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
